d5af082b1d97dcf68931581b63ed120a74ddd787
Pranay <pranaygohil2@gmail.com>
2024-10-01 00:38:00 +0530

    Title and Home Button Changed

--- client/src/UserHome.js
+++ client/src/UserHome.js
@@ -7,8 +7,8 @@ function UserHome() {
 
   return (
     <div className='container w-100 d-flex align-items-center justify-content-center' style={{height: '100vh'}}>
-      <button className='btn btn-primary btn-lg m-5' onClick={() => window.location.href = `${process.env.REACT_APP_ADMIN_URL}`}>Admin</button>
-      <button className='btn btn-primary btn-lg m-5' onClick={() => window.location.href = `${process.env.REACT_APP_MANAGER_URL}`}>Manager</button>
+      <button className='btn btn-primary btn-lg m-5' onClick={() => window.location.href = `${process.env.REACT_APP_ADMIN_URL}`}>Admin Panel</button>
+      <button className='btn btn-primary btn-lg m-5' onClick={() => window.location.href = `${process.env.REACT_APP_MANAGER_URL}`}>Manager Panel</button>
     </div>
   )
 }
